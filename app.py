59e750f38d7f00aee3264cc490dbf166ab89843e
captainawesome7 <91244918+captainawesome78@users.noreply.github.com>
2023-03-09 05:32:17 +0000

    added dynamic data and API route

--- app.py
+++ app.py
@@ -1,10 +1,38 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
+UPLOADS = [
+  {
+    'id' : 1,
+    'title' : 'Resume',
+    'price' : 'Free',
+    'uploads' : 'Unlimited'
+    
+  },
+  {
+    'id' : 2,
+    'title' : 'Certificates',
+    'price' : 'Free',
+    'uploads' : 'Unlimited'
+  },
+  {
+    'id' : 3,
+    'title' : 'Recognitions',
+    'price' : 'Free',
+    'uploads' : 'Unlimited'
+  }
+
+]
+
 @app.route("/")
 def bismillah():
-  return render_template('home.html')
+  return render_template('home.html', uploads = UPLOADS)
+
+@app.route("/api/uploads")
+def list_uploads():
+  return jsonify(UPLOADS)
+  
 
 if __name__ == "__main__":
   app.run(host = "0.0.0.0", debug =True)
